2d76434363103c96184947fd224645426d46da18
Himans110 <himanshgoel110@gmail.com>
2025-08-09 01:28:05 +0700

    Time: 44 ms (32.49%) | Memory: 45.7 MB (8.47%) - LeetSync

---
+++ 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String largestMerge(String word1, String word2) {
+        StringBuilder ans = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < word1.length() && j < word2.length()) {
+            if (word1.substring(i).compareTo(word2.substring(j)) > 0) {
+                ans.append(word1.charAt(i));
+                i++;
+            } else {
+                ans.append(word2.charAt(j));
+                j++;
+            }
+        }
+        while (i < word1.length()) ans.append(word1.charAt(i++));
+        while (j < word2.length()) ans.append(word2.charAt(j++));
+        return ans.toString();
+    }
+}
