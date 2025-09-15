f01c5dc3cd4f3bac62db51ae5df11ed0951819ae
Himans110 <himanshgoel110@gmail.com>
2025-09-16 00:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (86.77%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -22,7 +22,9 @@ class Solution {
         return isBalanced(root.left) && isBalanced(root.right);
     }
     public int height(TreeNode root){
-        if(root == null) return -1;
+        if(root  == null){
+            return -1;
+        }
         int lh = height(root.left);
         int rh = height(root.right);
         return Math.max(lh,rh)+1;
