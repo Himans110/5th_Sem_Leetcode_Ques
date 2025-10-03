e4549db8048c7d30a6a4fa96fcd8edf24b64dc4d
Himans110 <himanshgoel110@gmail.com>
2025-10-03 10:47:20 +0530

    Time: 1 ms (48.12%) | Memory: 42.6 MB (83.82%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> l = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        fun(nums,0,l,ans);
+        return l;
+    }
+    public void fun(int nums[] , int idx , List<List<Integer>> l , List<Integer> ans){
+        if(idx == nums.length){
+            l.add(new ArrayList<>(ans));
+            return;
+        }
+        fun(nums, idx+1,l,ans);
+        ans.add(nums[idx]);
+        fun(nums, idx+1,l,ans);
+        ans.remove(ans.get(ans.size()-1));
+    }
+}
