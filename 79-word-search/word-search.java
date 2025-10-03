5ae3c8ab701d902b4109b19786a1897909c09d17
Himans110 <himanshgoel110@gmail.com>
2025-10-03 19:53:37 +0530

    Time: 218 ms (17.32%) | Memory: 45.3 MB (8.68%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,28 +1,27 @@
 class Solution {
-    public boolean exist(char[][] b, String w) {
-        int m = b.length, n = b[0].length;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(b[i][j] == w.charAt(0)){
-                    if(find(b,w,i,j,0)) return true;
+    public boolean exist(char[][] board, String word) {
+        for(int i = 0 ; i < board.length ; i++){
+            for(int j = 0 ; j < board[0].length ; j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(ispossible(board, word, i, j , 0)) return true;
                 }
             }
         }
         return false;
     }
-    public boolean find(char b[][], String w , int cr ,int cc, int idx){
-        if(idx == w.length()) return true;
-        if(cr < 0 || cr >= b.length || cc < 0 || cc >= b[0].length || w.charAt(idx) != b[cr][cc]) return false;
-        b[cr][cc] = '*';
-        int r[] = {-1,0,1,0};
-        int c[] = {0,-1,0,1};
+    public boolean ispossible(char board[][] , String word , int cr, int cc , int idx){
+        if(idx == word.length()) return true;
+        if(cr < 0 || cr >= board.length || cc < 0 || cc >= board[0].length || board[cr][cc] != word.charAt(idx)) return false;
+        board[cr][cc] = '*';
+        int r[] = {0, -1 , 0 , 1};
+        int c[] = {1, 0, -1, 0};
         for(int k = 0 ; k < c.length ; k++){
-            boolean ans = find(b, w, cr+r[k] , cc+c[k],idx+1);
-            if(ans == true){
+            boolean ans = ispossible(board, word, cr+r[k], cc+c[k], idx+1);
+           if(ans == true){
                 return true;
             }
         }
-        b[cr][cc] = w.charAt(idx);
+        board[cr][cc] = word.charAt(idx);
         return false;
     }
 }
