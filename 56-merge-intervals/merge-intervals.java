1bb4757745c8d659786e367957a971d54d51d137
Himans110 <himanshgoel110@gmail.com>
2025-10-03 11:54:31 +0530

    Time: 8 ms (84.80%) | Memory: 47.2 MB (6.23%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] merge(int[][] arr) {
-        List<int[]> ans = new ArrayList<>();
+        int idx = 0;
         Arrays.sort(arr, (a,b)->(a[0]-b[0]));
         int st = arr[0][0];
         int end = arr[0][1];
@@ -9,12 +9,21 @@ class Solution {
                 end = Math.max(end, arr[i][1]);
             }
             else{
-                ans.add(new int[]{st,end});
+                arr[idx][0] = st;
+                arr[idx][1] = end;
+                idx++;
                 st = arr[i][0];
                 end = arr[i][1];
             }
         }
-        ans.add(new int[]{st,end});
-        return ans.toArray(new int[ans.size()][]);
+        arr[idx][0] = st;
+        arr[idx][1] = end;
+        idx++;
+        int ans[][] = new int[idx][2];
+        for(int i= 0 ; i < idx ; i++){
+            ans[i][0] = arr[i][0];
+            ans[i][1] = arr[i][1];
+        }
+        return ans;
     }
 }
