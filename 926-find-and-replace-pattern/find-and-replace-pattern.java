9b7aee4e165e3eb804ea9d3b476af811a8ba6e73
Himans110 <himanshgoel110@gmail.com>
2025-10-10 22:45:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (89.81%) - LeetSync

---
+++ 926-find-and-replace-pattern/find-and-replace-pattern.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> list = new ArrayList<>();
+        for (String word : words) {
+            if (matches(word, pattern)) {
+                list.add(word);
+            }
+        }
+        return list;
+    }
+    private boolean matches(String word, String pattern) {
+        if (word.length() != pattern.length()) return false;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.indexOf(word.charAt(i)) != pattern.indexOf(pattern.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
