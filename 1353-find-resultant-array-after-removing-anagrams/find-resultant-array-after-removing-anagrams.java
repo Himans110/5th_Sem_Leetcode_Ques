671da6943302c2740a8c953c79150e93b0c97852
Himans110 <himanshgoel110@gmail.com>
2025-10-13 19:21:28 +0530

    Time: 1 ms (99.78%) | Memory: 45.4 MB (8.55%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -10,14 +10,12 @@ class Solution{
         return ans;
     }
     public boolean is_ana(String s1, String s2){
-        TreeMap<Character,Integer> map1 = new TreeMap<>();
-        TreeMap<Character,Integer> map2 = new TreeMap<>();
-        for(char i : s1.toCharArray()){
-            map1.put(i,map1.getOrDefault(i,0)+1);
-        }
-        for(char i : s2.toCharArray()){
-            map2.put(i,map2.getOrDefault(i,0)+1);
-        }
-        return map1.equals(map2);
+        if (s1.length() != s2.length()) return false;
+        int[] freq = new int[26];
+        for (char c : s1.toCharArray()) freq[c - 'a']++;
+        for (char c : s2.toCharArray()) freq[c - 'a']--;
+        for (int f : freq) if (f != 0) return false;
+
+        return true;
     }
 }
