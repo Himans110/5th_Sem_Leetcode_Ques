d08cdfee59bfe01a7fdee2e9576513380d200d42
Himans110 <himanshgoel110@gmail.com>
2025-10-15 22:09:11 +0530

    Time: 11 ms (37.71%) | Memory: 45.6 MB (71.53%) - LeetSync

--- 2447-merge-similar-items/merge-similar-items.java
+++ 2447-merge-similar-items/merge-similar-items.java
@@ -1,30 +1,16 @@
 class Solution {
     public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < items1.length; i++) {
-            int val = items1[i][0];
-            int wt = items1[i][1];
-            map.putIfAbsent(val, new ArrayList<>());
-            map.get(val).add(wt);
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int[] item : items1) {
+            map.put(item[0], map.getOrDefault(item[0], 0) + item[1]);
         }
-        for (int i = 0; i < items2.length; i++) {
-            int val = items2[i][0];
-            int wt = items2[i][1];
-            map.putIfAbsent(val, new ArrayList<>());
-            map.get(val).add(wt);
+        for (int[] item : items2) {
+            map.put(item[0], map.getOrDefault(item[0], 0) + item[1]);
         }
-        List<Map.Entry<Integer, List<Integer>>> l = new ArrayList<>(map.entrySet());
-        Collections.sort(l, (a, b) -> a.getKey() - b.getKey());
         List<List<Integer>> ans = new ArrayList<>();
-        for (Map.Entry<Integer, List<Integer>> entry : l) {
-            int key = entry.getKey();
-            int sum = 0;
-            for (int wt : entry.getValue()) {
-                sum += wt;
-            }
-            ans.add(Arrays.asList(key, sum));
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            ans.add(Arrays.asList(entry.getKey(), entry.getValue()));
         }
-
         return ans;
     }
 }
