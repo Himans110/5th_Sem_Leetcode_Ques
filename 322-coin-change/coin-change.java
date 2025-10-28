4d42d8509267b44058cc9a1fe850f1a13793cee4
Himans110 <himanshgoel110@gmail.com>
2025-10-28 12:35:26 +0530

    Time: 37 ms (12.54%) | Memory: 45 MB (33.21%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -5,17 +5,20 @@ class Solution {
             Arrays.fill(row, -1);
         }
         int ans = fun(coins, amount , 0 , dp);
-        return ans == (int)1e9 ? -1 : ans;
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
     public int fun(int []arr, int amount , int idx, int [][]dp){
-        if(amount == 0) return 0;
-        if(idx == arr.length) return (int)1e9;
-        if(dp[idx][amount]!= -1) return dp[idx][amount];
-        int take = (int)1e9;
-        if(arr[idx] <= amount){
-            take = 1+fun(arr, amount-arr[idx], idx, dp);
+        if (amount == 0) return 0;
+        if (idx == arr.length) return Integer.MAX_VALUE;
+        if (dp[idx][amount] != -1) return dp[idx][amount];
+
+        int take = Integer.MAX_VALUE;
+        if (arr[idx] <= amount) {
+            int sub = fun(arr, amount - arr[idx], idx, dp);
+            if (sub != Integer.MAX_VALUE)  //  prevent overflow
+                take = 1 + sub;
         }
-        int skip = fun(arr, amount, idx+1, dp);
-        return dp[idx][amount] = Math.min(take,skip);
+        int skip = fun(arr, amount, idx + 1, dp);
+        return dp[idx][amount] = Math.min(take, skip);
     }
 }
