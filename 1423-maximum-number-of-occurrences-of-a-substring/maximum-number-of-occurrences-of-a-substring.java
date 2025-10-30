6e7f915d5c204dbd8e5468789c13f2a32aebc0dd
Himans110 <himanshgoel110@gmail.com>
2025-10-30 11:33:31 +0530

    Time: 1051 ms (5.00%) | Memory: 165 MB (5.00%) - LeetSync

---
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxFreq(String s, int maxl, int min, int max) {
+        HashMap<String , Integer> map = new HashMap<>();
+        for (int i = min; i <= max; i++) {
+            for (int j = 0; j <= s.length() - i; j++) {
+                String m = s.substring(j, j + i);
+                if (is_valid(m, maxl)) map.put(m, map.getOrDefault(m, 0)+1);
+            }
+        }
+        int maxf = 0;
+        for(String i : map.keySet()){
+            maxf= Math.max(maxf, map.get(i));
+        }
+        return maxf;
+    }
+    public boolean is_valid(String m , int maxf){
+        HashSet<Character> set = new HashSet<>();
+        for (char c : m.toCharArray()) {
+            set.add(c);
+            if (set.size() > maxf) return false;
+        }
+        return true;
+    }
+}
