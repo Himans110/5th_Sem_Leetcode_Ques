ca3fd0fb77881dca517bf46bceca141c2d6c0e09
Himans110 <himanshgoel110@gmail.com>
2025-11-03 22:00:02 +0530

    Time: 3 ms (85.45%) | Memory: 83.3 MB (5.45%) - LeetSync

--- 2591-frog-jump-ii/frog-jump-ii.java
+++ 2591-frog-jump-ii/frog-jump-ii.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i = 2 ; i < s.length ; i++){
             ans = Math.max(ans, s[i]- s[i-2]);
         }
-        for(int i = 3 ; i < s.length ; i++){
-            ans = Math.max(ans, s[i]- s[i-2]);
-        }
+        // for(int i = 3 ; i < s.length ; i++){
+        //     ans = Math.max(ans, s[i]- s[i-2]);
+        // }
         return ans;
     }
 }
