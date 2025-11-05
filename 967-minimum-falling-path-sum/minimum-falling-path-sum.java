6cdc4de7c5a51fcd049d2d7fa3abdf0d8e5e2b69
Himans110 <himanshgoel110@gmail.com>
2025-11-06 00:24:02 +0530

    Time: 1 ms (99.83%) | Memory: 47.3 MB (5.43%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,29 +1,28 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int ans = Integer.MAX_VALUE;
         int dp[][] = new int[matrix.length][matrix[0].length];
-        for(int a[] : dp){
-            Arrays.fill(a,-66666);
+        for(int i [] : dp){
+            Arrays.fill(i, 105);
         }
-        for (int col = 0; col < matrix[0].length; col++){
-            ans = Math.min(ans, Minimum_Falling_Path(matrix, 0, col,dp));
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0 ; i < matrix.length ; i++){
+            ans = Math.min(ans, fun(matrix,0,i ,dp));
         }
         return ans;
     }
-    public static int Minimum_Falling_Path(int [][] matrix, int cr, int cc, int dp[][]){
-        if (cc < 0 || cc >= matrix[0].length){
+    public int fun(int matrix[][] , int cr, int cc, int dp[][]){
+        if (cr >= matrix.length || cc < 0 || cc >= matrix[0].length) {
             return Integer.MAX_VALUE;
         }
-        if (cr == matrix.length - 1){
+        if(cr == matrix.length-1){
             return matrix[cr][cc];
         }
-        if(dp[cr][cc] != -66666){
-            return dp[cr][cc];
-        }
-        int ld = Minimum_Falling_Path(matrix, cr+1, cc-1,dp);
-        int rd = Minimum_Falling_Path(matrix, cr+1, cc+1,dp);
-        int d = Minimum_Falling_Path(matrix,cr+1,cc,dp);
-        return dp[cr][cc] = matrix[cr][cc] + Math.min(d, Math.min(ld, rd));
-
+        if(dp[cr][cc] != 105) return dp[cr][cc];
+        int l =fun(matrix, cr+1, cc-1, dp);
+        int s = fun(matrix, cr+1, cc, dp);
+        int r = fun(matrix, cr+1, cc+1 , dp);
+        int minBelow = Math.min(l, Math.min(s, r));
+        if (minBelow == Integer.MAX_VALUE) return matrix[cr][cc];
+        return dp[cr][cc] = matrix[cr][cc] + minBelow;
     }
 }
