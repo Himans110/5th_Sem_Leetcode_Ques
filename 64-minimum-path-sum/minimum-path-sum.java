ec82dbb0fb273689ea9592de814bce296a4efa92
Himans110 <himanshgoel110@gmail.com>
2025-11-05 15:29:09 +0530

    Time: 1 ms (99.76%) | Memory: 50.4 MB (5.05%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,23 +1,23 @@
 class Solution {
     public int minPathSum(int[][] grid) {
         int dp[][] = new int[grid.length][grid[0].length];
-        for(int a[] : dp){
-            Arrays.fill(a,-1);
+        for(int i[]:dp){
+            Arrays.fill(i , -1);
         }
-        return Minimum_Path(grid,0,0,dp);
+        return fun(grid, 0 , 0 , dp);
     }
-    public static int Minimum_Path(int[][] grid, int cr, int cc, int dp[][]){
+    public int fun(int grid[][] , int cr , int cc , int dp[][]){
+        if(cc >= grid[0].length || cr >= grid.length){
+            return Integer.MAX_VALUE;
+        }
         if(cc == grid[0].length-1 && cr == grid.length-1){
             return grid[cr][cc];
-        }
-        if(cc == grid[0].length || cr == grid.length){
-            return Integer.MAX_VALUE;
         }
         if(dp[cr][cc]!= -1){
             return dp[cr][cc];
         }
-        int d = Minimum_Path(grid, cr + 1, cc,dp);
-        int r = Minimum_Path(grid, cr, cc + 1,dp);
+        int d = fun(grid, cr + 1, cc,dp);
+        int r = fun(grid, cr, cc + 1,dp);
         return dp[cr][cc] = Math.min(d, r) + grid[cr][cc];
     }
 }
