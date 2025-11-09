e053aee16878195c5e8aedf6551cc171932f2659
Himans110 <himanshgoel110@gmail.com>
2025-11-09 21:46:11 +0530

    Time: 1 ms (90.35%) | Memory: 42 MB (18.76%) - LeetSync

---
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        if(num1 == 0 || num2 == 0) return 0;
+        if(num1 == num2) return 1;
+        int ans = 0;
+        while(num1 != num2){
+            if(num1 > num2){
+                num1 -= num2;
+            }
+            else num2 -= num1;
+            ans++;
+        }
+        ans += 1;
+        return ans;
+    }
+}
