75805a76e08a208dfc9f2f6ce77688880561d375
Himans110 <himanshgoel110@gmail.com>
2025-11-12 19:49:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (5.21%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,16 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
-        return Math.max(getMax(nums, 0, nums.length - 2), getMax(nums, 1, nums.length - 1));        
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        Integer[] dp1 = new Integer[n];
+        Integer[] dp2 = new Integer[n];
+        return Math.max(fun(nums, 0, n - 2, dp1), fun(nums, 1, n - 1, dp2));
     }
-    private int getMax(int[] nums, int start, int end) {
-        int prevRob = 0, maxRob = 0;
-        for (int i = start; i <= end; i++) {
-            int temp = Math.max(maxRob, prevRob + nums[i]);
-            prevRob = maxRob;
-            maxRob = temp;
-        }
 
-        return maxRob;
+    private int fun(int[] nums, int idx, int end, Integer[] dp) {
+        if (idx > end) return 0;
+        if (dp[idx] != null) return dp[idx];
+        int take = nums[idx] + fun(nums, idx + 2, end, dp);
+        int notTake = fun(nums, idx + 1, end, dp);
+        return dp[idx] = Math.max(take, notTake);
     }    
 }
