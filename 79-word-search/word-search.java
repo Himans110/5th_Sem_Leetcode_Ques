10790b555b2bfbea19ee9ed14894d2a0ff0ebb42
Himans110 <himanshgoel110@gmail.com>
2025-12-24 21:16:35 +0530

    Time: 224 ms (15.67%) | Memory: 47 MB (5.35%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for(int i = 0 ; i < board.length ; i++){
-            for(int j = 0 ; j < board[0].length ; j++){
+        for(int i = 0 ; i< board.length ; i++){
+            for(int j = 0 ; j< board[0].length ; j++){
                 if(board[i][j] == word.charAt(0)){
-                    if(ispossible(board, word, i, j , 0)) return true;
+                    if(fun(board,i,j ,word, 0)) return true;
                 }
             }
         }
         return false;
     }
-    public boolean ispossible(char board[][] , String word , int cr, int cc , int idx){
-        if(idx == word.length()) return true;
-        if(cr < 0 || cr >= board.length || cc < 0 || cc >= board[0].length || board[cr][cc] != word.charAt(idx)) return false;
-        board[cr][cc] = '*';
+    public boolean fun(char board[][], int cr, int cc, String word, int idx){
+        if(idx == word.length()){
+            return true;
+        }
+        if(cr < 0 || cc <0 ||cr >= board.length || cc >= board[0].length || board[cr][cc] != word.charAt(idx)) return false;
         int r[] = {0, -1 , 0 , 1};
         int c[] = {1, 0, -1, 0};
-        for(int k = 0 ; k < c.length ; k++){
-            boolean ans = ispossible(board, word, cr+r[k], cc+c[k], idx+1);
-           if(ans == true){
-                return true;
-            }
+        board[cr][cc] = '*';
+        for(int i = 0 ; i < 4 ; i++){
+            boolean ans = fun(board, cr+r[i], cc+c[i], word, idx+1);
+            if(ans == true) return true;
         }
         board[cr][cc] = word.charAt(idx);
         return false;
