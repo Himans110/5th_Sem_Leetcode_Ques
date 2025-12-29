83536f3924735c9e4323fdfd9efb6b2d241775ae
Himans110 <himanshgoel110@gmail.com>
2025-12-29 17:24:53 +0700

    Time: 0 ms (100.00%) | Memory: 43 MB (19.13%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,27 +1,11 @@
 class Solution {
-    public int subsetXORSum(int[] arr) {
-        List<List<Integer>> a = new ArrayList<>();
-        List<Integer> a1 = new ArrayList<>();
-        subset(arr,0,a1,a);
-        int sum = 0;
-        for(int i = 0 ; i < a.size() ; i++){
-            sum += find(a.get(i));
-        }
-        return sum;
+    public int subsetXORSum(int[] arr){
+        return helper(arr, 0, 0);        
     }
-    public static void subset(int nums[],int start,List<Integer> a1,List<List<Integer>> a){
-        a.add(new ArrayList<>(a1));
-        for (int i = start; i < nums.length; i++){
-            a1.add(nums[i]);
-            subset(nums, i + 1, a1, a);
-            a1.remove(a1.size() - 1);
-        }
-    }
-    public int find(List<Integer> a){
-        int xor = 0;
-        for (Integer num : a) {
-            xor ^= num;  
-        }
-        return xor;
+    public int helper(int[] a, int i, int x){
+        if(i == a.length) return x;
+        int t = helper(a, i+1, x ^ a[i]);
+        int nt = helper(a, i+1, x);
+        return t+nt;
     }
 }
