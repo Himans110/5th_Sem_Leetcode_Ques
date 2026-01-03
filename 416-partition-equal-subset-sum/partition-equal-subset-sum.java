6dc3d272753e2834dd188ef5c9f1da2286e3471c
Himans110 <himanshgoel110@gmail.com>
2026-01-03 16:41:31 +0700

    Time: 96 ms (19.97%) | Memory: 72.7 MB (27.88%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,24 +1,17 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int i : nums){
-            sum += i;
-        }
-        int n = nums.length;
-        Boolean dp[][] = new Boolean[n][sum/2 + 1];
-       if(sum % 2 == 0) return fun(n-1,sum/2, nums, dp);
-       return false;
+        for(int i : nums){sum += i;}
+        if(sum%2!=0) return false;
+        Boolean[][] dp = new Boolean [nums.length][(sum/2)+1];
+        return fun(nums, 0, sum/2, dp);
     }
-    public static boolean fun(int idx, int target, int arr[], Boolean dp[][]) {
-        if (target == 0) return true;
-        if (idx == 0) return arr[0] == target;
-
-        if (dp[idx][target] != null) return dp[idx][target];
-
-        boolean notTake = fun(idx - 1, target, arr, dp);
-        boolean take = false;
-        if (target >= arr[idx]) take = fun(idx - 1, target - arr[idx], arr, dp);
-
-        return dp[idx][target] = notTake || take;
+    public boolean fun(int nums[], int idx , int target, Boolean [][]dp){
+        if(target == 0) return true;
+        if(idx >= nums.length || target<0) return false;
+        if(dp[idx][target] != null) return dp[idx][target];
+        boolean pick = fun(nums, idx+1, target-nums[idx] , dp);
+        boolean not = fun(nums, idx+1, target, dp);
+        return dp[idx][target] = pick||not;
     }
 }
