40fdb838cbe827de656f48a16f59ff8ea07ac618
Himans110 <himanshgoel110@gmail.com>
2026-01-21 20:54:28 +0700

    Time: 2466 ms (19.76%) | Memory: 44 MB (90.00%) - LeetSync

---
+++ 1511-count-number-of-teams/count-number-of-teams.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numTeams(int[] nums) {
+        int ans = 0;
+        for(int i = 0 ; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                for(int k = j+1 ; k< nums.length; k++){
+                    if((nums[i] < nums[j] && nums[j] < nums[k]) || (nums[i] > nums[j] && nums[j] > nums[k]) ) ans++;
+                    
+                }
+            }
+        }
+        return ans;
+    }
+}
