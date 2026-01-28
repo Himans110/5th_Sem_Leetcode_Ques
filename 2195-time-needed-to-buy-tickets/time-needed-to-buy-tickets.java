0c2716918f1d285ba63a2b6fbb47969ab161efce
Himans110 <himanshgoel110@gmail.com>
2026-01-28 13:24:39 +0700

    Time: 11 ms (15.86%) | Memory: 46.3 MB (14.26%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int timeRequiredToBuy(int[] arr, int k) {
+        Queue<int[]> q = new LinkedList<>();
+        int ans = 0;
+        for(int i = 0 ; i < arr.length; i++){
+            if(i == k && arr[i]-1 == 0){
+                return ans+1;
+            }
+           if(arr[i]-1 != 0) q.add(new int[]{arr[i]-1, i});
+            ans++;
+        }
+        while(!q.isEmpty()){
+            int [] r = q.poll();
+            int put = r[0]-1;
+            if(put == 0 && r[1] == k) break;
+            ans++;
+            if(put != 0) q.add(new int[]{put , r[1]});
+        }
+        return ans+1;
+    }
+}
