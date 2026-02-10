c31a00838d73a2d967d40035b25a2837a0a1eaac
Himans110 <himanshgoel110@gmail.com>
2026-02-10 18:31:27 +0700

    Time: 17 ms (20.39%) | Memory: 74.3 MB (95.85%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.java
+++ 1731-even-odd-tree/even-odd-tree.java
@@ -1,42 +1,48 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    List<List<Integer>> a1 = new ArrayList<>();
     public boolean isEvenOddTree(TreeNode root) {
-        helper(root,0);
-        for (int i = 0; i < a1.size(); i++) {
-            List<Integer> a = a1.get(i);
-            for (int j = 0; j < a.size(); j++) {
-                int val = a.get(j);
-                if (i % 2 == 0) {
-                    if (val % 2 == 0) return false;
-                    if (j > 0 && a.get(j) <= a.get(j - 1)) return false;
-                } else {
-                    if (val % 2 != 0) return false;
-                    if (j > 0 && a.get(j) >= a.get(j - 1)) return false;
+        if(root.val % 2 == 0) return false;
+        int level = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            List<Integer> l = new ArrayList<>();
+            int size = q.size();
+            while(size-- > 0){
+                TreeNode curr = q.poll();
+                l.add(curr.val);
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
                 }
             }
+            boolean ans = check(l, level);
+            if(!ans) return false;
+            level++;
         }
         return true;
     }
-    public void helper(TreeNode node,int level){
-        if(node==null) return;
-        if(a1.size()==level) a1.add(new ArrayList<>());
-        a1.get(level).add(node.val);
-        helper(node.left,level+1);
-        helper(node.right,level+1);
+    public boolean check(List<Integer> l , int level){
+        if(level % 2 == 0){
+            for(int i = 0 ; i < l.size()-1 ; i++){
+                if(l.get(i) % 2 != 0){
+                    if(l.get(i) >= l.get(i+1)) return false;
+                }else{
+                    return false;
+                }
+            }
+            if(l.get(l.size()-1) % 2 == 0) return false;
+        }else{
+            for(int i = 0 ; i < l.size()-1 ; i++){
+                if(l.get(i) % 2 == 0){
+                    if(l.get(i) <= l.get(i+1)) return false;
+                }else{
+                    return false;
+                }
+            }
+            if(l.get(l.size()-1) % 2 != 0) return false;
+        }
+        return true;
     }
 }
