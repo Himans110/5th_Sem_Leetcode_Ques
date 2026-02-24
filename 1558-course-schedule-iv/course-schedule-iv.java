6f314bfddd5a2618fc017dca5bf70f33090b85d8
Himans110 <himanshgoel110@gmail.com>
2026-02-24 12:52:22 +0700

    Time: 127 ms (21.21%) | Memory: 50.6 MB (72.25%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -1,7 +1,6 @@
 class Solution {
-    List<List<Integer>> graph;
-    public List<Boolean> checkIfPrerequisite(int n, int[][] arr, int[][] q) {
-        graph = new ArrayList<>();
+    public List<Boolean> checkIfPrerequisite(int n, int[][] arr, int[][] qu) {
+        List<List<Integer>> graph = new ArrayList<>();
         for(int i = 0; i < n ; i++){
             graph.add(new ArrayList<>());
         }
@@ -9,28 +8,26 @@ class Solution {
             int u = a[0], v = a[1];
             graph.get(u).add(v);
         }
-        List<Boolean> ans = new ArrayList<>();
-        for(int a[] : q){
-            int s = a[0], d = a[1];
-            ans.add(validPath(s,d));
-        }
-        return ans;
-    }
-    public boolean validPath(int s, int d) {
-        Queue<Integer> q = new LinkedList<>();
-        HashSet<Integer> visited = new HashSet<>();
-        q.add(s);
-        while(!q.isEmpty()){
-            int r = q.poll();
-            if(visited.contains(r)) continue;
-            visited.add(r);
-            if(r == d) return true;
-            for(int nbr : graph.get(r)){
-                if(!visited.contains(nbr)){
-                    q.add(nbr);
+        for(int i = 0; i < n ; i++){
+            Queue<Integer> q = new LinkedList<>();
+            HashSet<Integer> visited = new HashSet<>();
+            q.add(i);
+            while(!q.isEmpty()){
+                int r = q.poll();
+                if(visited.contains(r)) continue;
+                visited.add(r);
+                graph.get(i).add(r);
+                for(int nbr : graph.get(r)){
+                    if(!visited.contains(nbr)){
+                        q.add(nbr);
+                    }
                 }
             }
         }
-        return false;
+        List<Boolean> ans = new ArrayList<>();
+        for(int[] a : qu){
+            ans.add(graph.get(a[0]).contains(a[1]));
+        }
+        return ans;
     }
 }
