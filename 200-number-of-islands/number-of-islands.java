9180434c7d78ee4ffc9d78ba464dd5a0c20f62dd
Himans110 <himanshgoel110@gmail.com>
2026-03-16 12:13:58 +0700

    Time: 3 ms (86.77%) | Memory: 52.2 MB (66.49%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,23 +1,22 @@
 class Solution {
-    public int numIslands(char[][] g) {
+    public int numIslands(char[][] grid) {
         int count = 0;
-        for(int i = 0 ; i < g.length ; i++){
-            for(int j = 0 ; j < g[0].length ; j++){
-                if(g[i][j] == '1'){
-                    find(g,i,j);
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    find(grid, i, j);
                     count++;
                 }
             }
         }
         return count;
     }
-    public void find(char g[][] , int i , int j){
-        if(i <0 || i >= g.length || j < 0 || j >= g[0].length || g[i][j] != '1') return;
-        g[i][j] = '0';
-        find(g , i-1 , j);
-        find(g , i+1 , j);
-        find(g , i , j-1);
-        find(g , i , j+1);
+    public void find(char [][]grid, int i , int j){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        find(grid, i-1, j);
+        find(grid, i, j-1);
+        find(grid, i+1, j);
+        find(grid, i, j+1);
     }
-
 }
