ee479856a61e4a1b88d3039679387901f4e5c3a4
Himans110 <himanshgoel110@gmail.com>
2026-03-20 16:19:32 +0700

    Time: 28 ms (19.94%) | Memory: 54.1 MB (46.57%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,24 +1,23 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int dp[][] = new int[text1.length()][text2.length()];
-        for(int i[] : dp){
-            Arrays.fill(i, -1);
+    public int longestCommonSubsequence(String s1, String s2) {
+        int dp[][] = new int[s1.length()][s2.length()];
+        for(int d[] : dp){
+            Arrays.fill(d , -1);
         }
-        return lcs(text1, text2, 0 , 0, dp);
+        return solve(s1, s2 , 0 , 0, dp);
     }
-    public int lcs(String s1 , String s2 , int i , int j, int [][]dp){
-        if(i == s1.length() || j == s2.length()) return 0;
+    public int solve(String s1, String s2, int i , int j, int [][]dp){
+        if(i >= s1.length() || j >= s2.length()) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         int ans = 0;
         if(s1.charAt(i) == s2.charAt(j)){
-            ans  = 1+lcs(s1,s2,i+1,j+1,dp);
+            ans = 1+solve(s1, s2 , i+1, j+1, dp);
         }
         else{
-            int f = lcs(s1, s2 ,i+1, j, dp);
-            int s = lcs(s1,s2, i, j+1, dp);
-            ans = Math.max(f,s);
+            int a = solve(s1, s2 , i+1, j, dp);
+            int b = solve(s1 , s2 , i, j+1, dp);
+            ans += Math.max(a,b);
         }
         return dp[i][j] = ans;
-
     }
 }
