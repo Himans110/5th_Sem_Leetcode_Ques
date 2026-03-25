4a24413b33cb1766bcfafbc584d446b42c4758ca
Himans110 <himanshgoel110@gmail.com>
2026-03-25 21:36:22 +0700

    Time: 4 ms (73.20%) | Memory: 44.6 MB (71.52%) - LeetSync

--- 4013-merge-close-characters/merge-close-characters.java
+++ 4013-merge-close-characters/merge-close-characters.java
@@ -2,29 +2,22 @@ class Solution {
     public String mergeCharacters(String s, int k) {
         StringBuilder str = new StringBuilder(s);
         int i = 0;
-
         while (i < str.length()) {
             char ch = str.charAt(i);
             boolean flag = false;
-
             int limit = Math.min(i + k + 1, str.length());
-
             for (int j = i + 1; j < limit; j++) {
                 if (ch == str.charAt(j)) {
                     str.deleteCharAt(j);
                     flag = true;
-
-                    // 🔥 FIX: restart पूरी string
                     i = 0;
                     break;
                 }
             }
-
             if (!flag) {
                 i++;
             }
         }
-
         return str.toString();
     }
 }
